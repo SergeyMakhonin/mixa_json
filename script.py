761985fc72eba52fa4f84342f3c94598587d5ea2
fox87 <smchonin@gmail.com>
2019-06-30 20:09:58 +0300

    implemented json parsing into custom class with paths representation

--- script.py
+++ script.py
@@ -1,8 +1,5 @@
 import json
-import pprint
-
-path = 'live.json'
-
+import sys
 
 
 class RootObject:
@@ -18,4 +15,58 @@ class RootObject:
             self.json_data = json.loads(data)
 
     def parse_json_data(self):
-        for key, value in self.parsed_json:
+        for key in self.json_data:
+            # get all sport types on this level
+            self.sports.append({self.json_data[key]['name']: {self.json_data[key]['id']}})
+
+            # get all league types on this level
+            for league_id in self.json_data[key]['league']:
+                self.league.append({self.json_data[key]['league'][league_id]['name']:
+                                        (key,
+                                         'league',
+                                         self.json_data[key]['league'][league_id])
+                                    }
+                                   )
+
+                # get all events per league
+                for event_id in self.json_data[key]['league'][league_id]['event']:
+                    self.event.append({self.json_data[key]['league'][league_id]['event'][event_id]['name']:
+                                           (key,
+                                            'league',
+                                            self.json_data[key]['league'][league_id],
+                                            'event',
+                                            self.json_data[key]['league'][league_id]['event'][event_id]
+                                            )
+                                       }
+                                      )
+
+    def return_all_sports(self):
+        print('Sport types available:')
+        sport_names = []
+        for sport in self.sports:
+            print([*sport][0])
+            sport_names.append([*sport][0])
+        return sport_names
+
+
+if __name__ == '__main__':
+    # set path
+    # if not path provided set path to 'live.json'
+    try:
+        path = sys.argv[1]
+    except IndexError as e:
+        path = 'live.json'
+        print('Exception: %s.' % e)
+        print('No input file provided, using default "%s".' % path)
+
+    # init RootObject that will contain all JSON data parsed
+    rt = RootObject()
+
+    # load JSON file
+    rt.load_json_data(path)
+
+    # parse loaded JSON file
+    rt.parse_json_data()
+
+    # return all sports available
+    rt.return_all_sports()
