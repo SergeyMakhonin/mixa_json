480570d3f4e90d2b6a9f5be886a934fa820e9bab
fox87 <smchonin@gmail.com>
2019-10-15 03:04:59 +0300

    UI revamp:  - made changes to UI

--- AbominationVisage/vigorous_glance.py
+++ AbominationVisage/vigorous_glance.py
@@ -11,48 +11,53 @@ except FileNotFoundError as e:
     sys.exit(1)
 
 layout = [
-    [PySimpleGUI.Text('Outcomes:\n', font=('Calibri', 11), size=(30, 1), justification='left')],
-    [PySimpleGUI.Text('No data', key='outcomes', font=('Calibri', 11), size=(30, 3))],
-    [PySimpleGUI.Combo(values=['no data'],
+    [PySimpleGUI.Text('Ставки main:', key='outcomes_label_main', font=('Calibri', 11), size=(30, 1), justification='left'),
+     PySimpleGUI.Text('Ставки nextgoal:', key='outcomes_label_nextgoal', font=('Calibri', 11), size=(30, 1), justification='right')],
+    [PySimpleGUI.Text('', key='main', font=('Calibri', 11), size=(30, 3), justification='left'),
+     PySimpleGUI.Text('', key='nextgoal', font=('Calibri', 11), size=(30, 3), justification='right')],
+    [PySimpleGUI.Combo(values=['Выбор матча'],
                        enable_events=True,
                        readonly=True,
                        key='topic_combobox',
                        auto_size_text=True,
-                       size=(30, 3))],
-    [PySimpleGUI.Combo(values=['no data'],
-                       enable_events=True,
-                       readonly=True,
-                       key='bet_types_combobox',
-                       auto_size_text=True,
-                       size=(30, 3))]
+                       size=(60, 3))]
 ]
 
 # make window and a client for the server
-window = PySimpleGUI.Window('Client', layout, grab_anywhere=False)
+window = PySimpleGUI.Window('Твои данные для матча v0.3', layout, grab_anywhere=False)
 s = xmlrpc.client.ServerProxy(config['server_address'])
 
 
-def write_to_file(path, data_string):
+def write_to_file(path, bet, data_string):
     # write chosen bet types to a file
     try:
-        with open(path, 'w+') as bt:
+        with open(path % bet, 'w+') as bt:
             bt.write(data_string)
     except FileNotFoundError as e:
         PySimpleGUI.PopupError('Path does not exist: {e}'.format(e=e))
         sys.exit(1)
 
 
+def request_outcomes(chosen_topic):
+    # get bet types
+    bet_types = []
+    for topic in topics:
+        bet_types.extend(s.return_bet_types(topic).split(','))
+
+    # get outcomes for chosen topic and bet type
+    outcomes_dict = {}
+    for bet_type in bet_types:
+        outcomes_dict[bet_type] = s.return_outcomes(chosen_topic, bet_type)
+    return bet_types, outcomes_dict
+
+
 while True:
     # request all topics available and put them in the combobox
     try:
         topics = s.return_all_topics().split(',')
-        bet_types = []
-        for topic in topics:
-            bet_types.extend(s.return_bet_types(topic).split(','))
 
         # load topics to UI
         window.Element('topic_combobox').Values = topics
-        window.Element('bet_types_combobox').Values = bet_types
     except ConnectionRefusedError as e:
         PySimpleGUI.PopupError('Server is not running: {e}'.format(e=e))
         sys.exit(1)
@@ -62,17 +67,15 @@ while True:
     if event in (None, 'Exit'):
         break
 
-    elif event == 'bet_types_combobox':
+    elif event == 'topic_combobox':
         # get chosen bet type and topic from combobox
         chosen_topic = window.Element('topic_combobox').Get()
-        chosen_bet_type = window.Element('bet_types_combobox').Get()
 
         # get outcomes for chosen topic and bet type
-        outcomes = s.return_outcomes(chosen_topic, chosen_bet_type)
+        bet_types, outcomes = request_outcomes(chosen_topic)
 
-        # show outcomes on their label
-        window.Finalize()
-        window.Element('outcomes').Update(outcomes)
-
-        write_to_file(config['outcomes_file'], outcomes)
+        for bet in bet_types:
+            # show outcomes on their label
+            window.Element(bet).Update(outcomes[bet])
+            write_to_file(config['outcomes_file'], bet, outcomes[bet])
 window.close()
