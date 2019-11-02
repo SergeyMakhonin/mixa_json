e46a25adc19a60807cdcd6119e7eb4c1d9b6c73a
fox87 <smchonin@gmail.com>
2019-11-02 17:14:12 +0300

    Opta reaper:  - latest changes for via-browse interaction

--- OptaReaper/opta_web_tools.py
+++ OptaReaper/opta_web_tools.py
@@ -40,3 +40,29 @@ def opta_login(driver, config):
         pass_field.send_keys(Keys.RETURN)
     except NoSuchElementException as e:
         log('Unable to find element: {e}'.format(e=e))
+
+
+def opta_select_match(driver, config, competition, date, match):
+    # competition select
+    competition_select_dropbox_id = 'competition-select-lm3_0'
+
+    # wait for dropbox and find it
+    wretched_wait(driver, config, competition_select_dropbox_id)
+    competition_select_dropbox = driver.find_element_by_id(competition_select_dropbox_id)
+
+    # get its options and find desired ones to reap
+    competition_options = competition_select_dropbox.find_elements_by_tag_name('option')
+    for competition_option in competition_options:
+        if competition_option.get_attribute('text') in config['opta_reaper']['competition_to_reap']:
+            competition_option.click()
+            log('Competiton chosen: {c}'.format(c=config['opta_reaper']['competitions_to_reap']))
+
+        # wait for matches date input
+        matches_date_input_id = 'datepicker'
+        wretched_wait(driver, config, matches_date_input_id)
+        matches_date_picker = driver.find_element_by_id(matches_date_input_id)
+        for date in config['opta_reaper']['matche_dates']:
+            matches_date_picker.clear()
+            matches_date_picker.send_keys(date)
+            matches_date_picker.send_keys(Keys.RETURN)
+            log('Chosen date: {date}'.format(date=date))
--- OptaReaper/stabber.py
+++ OptaReaper/stabber.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-from OptaWebTools import opta_login
+from OptaReaper.opta_web_tools import opta_login, opta_select_match
 from logging_and_configuration import json_reader
 
 config = json_reader('../config.json')
@@ -9,6 +9,6 @@ driver = webdriver.Firefox()
 driver.get(config['opta_reaper']['opta_host'])
 
 opta_login(driver, config)
+opta_select_match(driver, config)
 
 driver.close()
-
