8e4296eaec7bec0d329e293a19596525b099c198
fox87 <fox.87@live.ru>
2020-08-14 02:54:38 +0300

    DevourersEater.py Added docstring.

--- DevourersEater.py
+++ DevourersEater.py
@@ -4,6 +4,11 @@ from logging_and_configuration import json_reader
 
 
 class DevourersEater(JsonBlazer, StorageRavager):
+    """
+    This class is a proxy class to ease XML-RPC interaction.
+       It mixes classes methods into one class and when initialized,
+       all methods of mixed classes are available via XML-RPC server in one registration.
+    """
     def __init__(self, config):
         super(DevourersEater, self).__init__(config)
         self.config = config
